bab24080d946b25f1f3bc6c587b44249a0b18c20
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-03 02:32:16 -0700

    Fix TailwindCSS config

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App bg-red-600">
+            <div className="bg-yellow-600">{count}</div>
+            <button className="text-red-500 bg-yellow-600">a11a</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    mode: 'jit',
-    purge: ['./index.html','./src/**/*.{jsx?,tsx?}'],
+    purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
