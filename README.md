a945d6683bc95bc976cc5e4af99972e3a4ba4d60
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-03 02:49:30 -0700

    Initial layout play

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Illusion of three-dimensional shapes.
 #### Inspired by
 
 [neumorphism](https://github.com/adamgiebl/neumorphism)
+[fancy-border-radius](https://9elements.github.io/fancy-border-radius/)
--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
 
     return (
-        <div className="App bg-green-50 h-screen">
+        <div className="App bg-green-50 h-screen flex place-items-center justify-center">
             <div className="relative">
-                <div className="w-20 h-20 bg-purple-400"></div>
-                <div className="w-20 h-20 bg-purple-500"></div>
-                <div className="w-20 h-20 bg-purple-600"></div>
+                <div className="w-20 h-20 bg-purple-400 threed" style={{borderRadius: '35% 65% 67% 33% / 21% 30% 70% 79%'}}></div>
+                <div className="w-20 h-20 bg-purple-500 threed" style={{borderRadius: '35% 65% 67% 33% / 21% 30% 70% 79%'}}></div>
+                <div className="w-20 h-20 bg-purple-600 threed" style={{borderRadius: '35% 65% 67% 33% / 21% 30% 70% 79%'}}></div>
             </div>
         </div>
     );
