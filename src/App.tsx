ab9c718cd60dfd053429e5f4ccd6ded4189e725d
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-03 20:56:15 -0700

    Add use of useTweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-import Tweakpane from 'tweakpane';
+import { useTweaks } from 'use-tweaks';
 
 function App() {
-    const pane = useRef<Tweakpane>();
-
-    const [PARAMS, setPARAMS] = useState({
+    const { tint } = useTweaks('Test', {
         color: '',
         tint: '#22aaeeff',
-    })
+    });
 
-    useEffect(() => {
-        let panels = new Tweakpane({
-            title: 'Controls'
-        });
-        panels.addInput(PARAMS, 'color');
-        panels.addInput(PARAMS, 'tint').on('change', (e) => {
-            setPARAMS((v) => ({...v, tint: e.value}));
-        });
-
-        panels.on('change', (ev) => {
-            console.log(`changed: ${ev.presetKey} = ${ev.value}`);
-        });
-
-        pane.current = panels;
-        return () => {
-            pane.current = undefined;
-        }
-    }, []);
-
-    useEffect(() => {
-        setTimeout(() => {
-            console.log('-----------');
-            
-            setPARAMS((v) => ({...v, tint: '#000'}));
-            //pane.current?.refresh();
-        }, 2000);
-    }, []);
-
-    useEffect(() => {
-        console.log('===========', {PARAMS}, {pane: pane.current });
-        pane.current?.refresh();
-    }, [PARAMS]);
+    console.log({tint});
 
     return (
         <div className="App bg-green-50 h-screen flex place-items-center justify-center">
             <div className="relative w-96 h-96">
-                <div className="w-20 h-20 bg-purple-600 threed box1 absolute top-0 left-0" style={{backgroundColor: `${PARAMS.tint}`}}></div>
+                <div className="w-20 h-20 bg-purple-600 threed box1 absolute top-0 left-0" style={{ backgroundColor: `${tint}` }}></div>
                 <div className="w-20 h-20 bg-purple-400 threed box1 absolute top-0 right-0" style={{}}></div>
                 <div className="w-20 h-20 bg-purple-500 threed box1 absolute bottom-0 right-0" style={{}}></div>
             </div>
