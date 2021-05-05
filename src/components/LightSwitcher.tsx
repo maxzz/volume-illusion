661019fd4b330dc2785ef3687c7dc3e6fc0a44ec
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-04 21:23:25 -0700

    Bug: Tailwind does not support string interpolation

--- src/components/LightSwitcher.tsx
+++ src/components/LightSwitcher.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function LightSwitcher({source, setSource}: {source: number, setSource: (n: number) => void}) {
+    let gradient = source === 3 ? 'bl' : source === 2 ? 'br' : source === 1 ? 'tr' : 'tl';
+
     return (
         <div className="grid grid-cols-3 text-gray-100">
-            <button className="col-span-2 min-w-[64px] focus:outline-none">
+            <div className="col-span-2 min-w-[64px]">
                 <svg
-                    className={`${source === 0 ? 'fill-[#fefe1f] stroke-[#654900]' : 'opacity-30'} h-6 w-6 transform rotate-[135deg] translate-x-[100%]`}
+                    className={`${source === 0 ? 'fill-[#fefe1f] stroke-[#654900]' : 'opacity-30'} h-6 w-6 transform rotate-[135deg] translate-x-[100%] cursor-pointer`}
                     fill="none" viewBox="0 0 24 24" stroke="currentColor"
                     onClick={() => setSource(0)}
                 >
@@ -14,10 +16,10 @@ function LightSwitcher({source, setSource}: {source: number, setSource: (n: numb
                         d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
                     />
                 </svg>
-            </button>
-            <button className=" min-w-[64px] focus:outline-none">
+            </div>
+            <div className=" min-w-[64px]">
                 <svg
-                    className={`${source === 1 ? 'fill-[#fefe1f] stroke-[#654900]' : 'opacity-30'} h-6 w-6 transform rotate-[225deg]`}
+                    className={`${source === 1 ? 'fill-[#fefe1f] stroke-[#654900]' : 'opacity-30'} h-6 w-6 transform rotate-[225deg] cursor-pointer`}
                     fill="none" viewBox="0 0 24 24" stroke="currentColor"
                     onClick={() => setSource(1)}
                 >
@@ -26,13 +28,13 @@ function LightSwitcher({source, setSource}: {source: number, setSource: (n: numb
                         d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
                     />
                 </svg>
-            </button>
+            </div>
 
-            <div className="h-6 w-6 px-4 col-span-3 justify-self-center border rounded"></div>
+            <div className={`h-6 w-6 px-4 col-span-3 justify-self-center border rounded bg-gradient-to-${gradient} from-yellow-400 via-red-500 to-pink-500`}></div>
 
-            <button className="col-span-2 min-w-[64px] focus:outline-none">
+            <div className="col-span-2 min-w-[64px]">
                 <svg
-                    className={`${source === 2 ? 'fill-[#fefe1f] stroke-[#654900]' : 'opacity-30'} h-6 w-6 transform rotate-[45deg] translate-x-[100%]`}
+                    className={`${source === 2 ? 'fill-[#fefe1f] stroke-[#654900]' : 'opacity-30'} h-6 w-6 transform rotate-[45deg] translate-x-[100%] cursor-pointer`}
                     fill="none" viewBox="0 0 24 24" stroke="currentColor"
                     onClick={() => setSource(2)}
                 >
@@ -41,10 +43,10 @@ function LightSwitcher({source, setSource}: {source: number, setSource: (n: numb
                         d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
                     />
                 </svg>
-            </button>
-            <button className=" min-w-[64px] focus:outline-none">
+            </div>
+            <div className=" min-w-[64px]">
                 <svg
-                    className={`${source === 3 ? 'fill-[#fefe1f] stroke-[#654900]' : 'opacity-30'} h-6 w-6 transform rotate-[-45deg]`}
+                    className={`${source === 3 ? 'fill-[#fefe1f] stroke-[#654900]' : 'opacity-30'} h-6 w-6 transform rotate-[-45deg] cursor-pointer`}
                     fill="none" viewBox="0 0 24 24" stroke="currentColor"
                     onClick={() => setSource(3)}
                 >
@@ -53,7 +55,7 @@ function LightSwitcher({source, setSource}: {source: number, setSource: (n: numb
                         d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
                     />
                 </svg>
-            </button>
+            </div>
         </div>
     );
 }
