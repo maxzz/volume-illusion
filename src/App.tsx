ea4144868a3bcc82f430fbc4459be04f97ea5a1b
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-04 20:43:50 -0700

    Code clean up

--- src/App.tsx
+++ src/App.tsx
@@ -85,24 +85,16 @@ function App() {
         '--radius': `${PARAMS.radius}px`,
     } as CSSProperties;
 
-    function handleLight(id: number) {
-        setSun(id);
-    }
-
-    function handleShape(id: number) {
-        setShape(id);
-    }
-
     return (
         <div className="h-screen flex flex-col place-items-center justify-between" style={{ backgroundColor: PARAMS.color }}>
             <div className="self-start mx-2 mt-2 p-4 pt-0 overflow-hidden rounded-lg w-44 bg-[#2f3137] space-y-2 border-l-[3px] border-[#3d4146] shadow-md">
                 <div className="">
                     <div className="bg-[#3d4146] -mx-4 px-3 py-1 mb-2 text-[#c8cad0] text-[11px] font-courier">Light source</div>
-                    <LightSwitcher source={sun} setSource={handleLight} />
+                    <LightSwitcher source={sun} setSource={setSun} />
                 </div>
                 <div className="">
                     <div className="bg-[#3d4146] -mx-4 px-3 py-1 mb-2 text-[#c8cad0] text-[11px] font-courier">Shape</div>
-                    <ShapeSwitcher shape={shape} setShape={handleShape} />
+                    <ShapeSwitcher shape={shape} setShape={setShape} />
                 </div>
             </div>
             <div className="flex-1 flex items-center">
