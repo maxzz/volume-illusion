118b910fc89f6d97ea264afe7bf20ab5f95d053e
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-05 18:01:10 -0700

    Fix: assets deployment path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
-export default (command: string) => {
+export default ({command}) => {
     return defineConfig({
-        base: command === 'build' ? '/volume-illusion/' : '',
+        base: command === 'build' ? '' : '',
+        // GitHub allows relative hosting path
+        // base: command === 'build' ? '/volume-illusion/' : '',
         plugins: [reactRefresh()]
     });
 };
